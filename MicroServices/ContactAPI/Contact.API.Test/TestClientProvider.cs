21bffb097f6c0960561f53c75f53e634060fa4d2
okan Altuntaş <okan_altuntas@hotmail.com.tr>
2022-02-18 14:27:13 +0300

    Report.API.Test created

---
+++ MicroServices/ContactAPI/Contact.API.Test/TestClientProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+
+namespace Contact.API.Test
+{
+    class TestClientProvider
+    {
+        public HttpClient Client { get; set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+
+        }
+    }
+}
